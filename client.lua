d1a70c45e1399b427b3359454a21937a4b501742
Hyz <grigore.filip12@gmail.com>
2023-11-17 14:59:24 +0200

    Added files
    0.01

---
+++ client.lua
@@ -0,0 +1,79 @@
+setText = function(text)
+    return SendNUIMessage({
+        type = "textUi",
+        toggle = true,
+        text = text 
+    })
+end 
+
+removeText = function()
+    return SendNUIMessage({
+        type = "textUi",
+        toggle = false
+    })
+end 
+
+local areas = {}
+addArea = function(area,coords,distance,functions)
+    if areas[area] then return end 
+
+    areas[area] = {
+        coords = coords,
+        distance = distance,
+        functions = functions
+    }
+end 
+
+removeArea = function(area)
+    if not areas[area] then return end 
+
+    areas[area] = nil 
+end 
+
+isNearArea = function()
+    local isNear,areaName = false,""
+
+    for k,v in pairs(areas) do 
+        if #(GetEntityCoords(PlayerPedId()) - v.coords.xyz) > v.distance then goto skip end 
+
+        isNear = true 
+        areaName = k 
+        break 
+        ::skip::
+    end 
+    return isNear,areaName 
+end 
+
+
+CreateThread(function()
+    local ticks = 500 
+    local lastArea = nil 
+    local inside = false 
+    while 1 do 
+        Wait(ticks)
+        ticks = 500 
+
+        local isNear,areaName = isNearArea()
+        if not isNear then 
+            if lastArea ~= nil then 
+                if areas[lastArea] then 
+                    areas[lastArea].functions.exit()
+                    lastArea = nil 
+                    goto skip 
+                end 
+            end 
+        end 
+
+        if areaName == lastArea then goto skip end 
+        
+        lastArea = areaName
+        areas[areaName].functions.enter()
+        ::skip::
+    end 
+end)
+
+exports("setText",setText)
+exports("removeText",removeText)
+
+exports("addArea",addArea)
+exports("removeArea",removeArea)
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Hyz"
+description "A lib made for hyz resources"
+
+client_scripts {
+    "client.lua"
+}
+
